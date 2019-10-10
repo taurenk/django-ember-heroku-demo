1f2e2f09022f01410048be29497f00dba119984f
Tauren Kristich <tauren.kristich@gmail.com>
2019-10-10 16:11:49 -0400

    - making a default home page

--- demosite/urls.py
+++ demosite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import health_check
+from .views import health_check, home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('health', health_check, name='health')
+    path('', home, name='home'),
+    path('health', health_check, name='health'),
 ]
--- demosite/views.py
+++ demosite/views.py
@@ -1,5 +1,14 @@
 from django.http import JsonResponse
 
+
+def home(request):
+    data = {
+        "results": {
+            "message": "welcome home"
+        }
+    }
+    return JsonResponse(data)
+
 def health_check(request):
     data = {
         "results": {
